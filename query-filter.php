37983dc2edd4a6ab7bf7f13a03d2735fb75a54f9
k1tesurfen <18611152+k1tesurfen@users.noreply.github.com>
2026-03-04 18:13:42 +0100

    v0.2.7 include the plugin checker correctly

--- query-filter.php
+++ query-filter.php
@@ -5,7 +5,7 @@
  * Description:       Filter blocks for the query loop utilising the interactivity API.
  * Requires at least: 6.6
  * Requires PHP:      8.0
- * Version:           0.2.6
+ * Version:           0.2.7
  * Author:            Human Made Limited and Kaith Menken
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -19,7 +19,7 @@ namespace HM\Query_Loop_Filter;
 const PLUGIN_FILE = __FILE__;
 const ROOT_DIR = __DIR__;
 
-require plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker.php';
+require plugin_dir_path(__FILE__) . '/plugin-update-checker/plugin-update-checker.php';
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
