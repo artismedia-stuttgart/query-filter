7edcf852cd6691bfd4791d32439c03102dd99e7d
k1tesurfen <18611152+k1tesurfen@users.noreply.github.com>
2026-03-04 16:34:31 +0100

    init am version

--- query-filter.php
+++ query-filter.php
@@ -1,21 +1,37 @@
 <?php
+
 /**
- * Plugin Name:       Query Loop Filters
+ * Plugin Name:       Query Loop Filters (modified)
  * Description:       Filter blocks for the query loop utilising the interactivity API.
  * Requires at least: 6.6
  * Requires PHP:      8.0
  * Version:           0.2.4
- * Author:            Human Made Limited
+ * Author:            Human Made Limited and Kaith Menken
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       query-filter
  *
  * @package           query-filter
  */
+
 namespace HM\Query_Loop_Filter;
 
 const PLUGIN_FILE = __FILE__;
 const ROOT_DIR = __DIR__;
+
+require plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$queryFilterUpdateChecker = PucFactory::buildUpdateChecker(
+  'https://github.com/artismedia-stuttgart/query-filter',
+  __FILE__,
+  'query-filter'
+);
+
+//Set the branch that contains the stable release.
+$queryFilterUpdateChecker->setBranch('main');
+$queryFilterUpdateChecker->setAuthentication('');
 
 require_once __DIR__ . '/inc/namespace.php';
 
